6934357f824122adce8f2f1d7ef51b2d5a5eddea
irem pekkıyak <81883200+irempekkiyak@users.noreply.github.com>
2023-05-09 19:51:13 +0300

    .

--- Assets/Scripts/CamFollowPlayer.cs
+++ Assets/Scripts/CamFollowPlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CamFollowPlayer : MonoBehaviour
 {
-    public Transform cameraTarget;
+    /*public Transform cameraTarget;
     public float sSpeed = 10f;
     public Vector3 dist;
     public Transform lookTarget;
@@ -14,5 +14,20 @@ public class CamFollowPlayer : MonoBehaviour
       Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed *Time.deltaTime);
       transform.position = sPos;
       transform.LookAt(lookTarget.position);
+    }*/
+
+    public Transform target;
+    private Vector3 offset;
+
+    private void start()
+    {
+      offset = transform.position - target.position;
     }
+
+    private void LateUpdate()
+    {
+      Vector3 newPos = new Vector3(transform.position.x, transform.position.y, offset.z + target.position.z);
+      transform.position = Vector3.Lerp(transform.position, newPos, 2 * Time.deltaTime);
+    }
+
 }
